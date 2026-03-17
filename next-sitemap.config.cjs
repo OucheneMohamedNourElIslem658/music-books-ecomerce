270769414b728683ce597e4c2034fd2403cc343e
monei <ouchenemohamednourelislam@gmail.com>
2026-03-17 11:32:35 +0100

    fixed sitemap

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,4 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://music-books-ecomerce.vercel.app',
+};
--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://music-books-ecomerce.vercel.app', // your live URL
-    generateRobotsTxt: true, // generate robots.txt
-    sitemapSize: 5000, // optional: max URLs per sitemap
-    changefreq: 'daily', // optional: default frequency
-    priority: 0.7, // optional: default priority
-    exclude: ['/secret-page'], // optional: pages you want to exclude
-};
